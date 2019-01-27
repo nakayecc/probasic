0b9c4f885aa906b6b91f17d4d570e361e7830c19
Michal Nabielec <michal.nabie@gmail.com>
2019-01-28 00:14:22 +0100

    Initial Commit

---
+++ idea.py
@@ -0,0 +1,58 @@
+import getopt
+import sys
+
+def savef(text):
+    try:
+        with open('ideabank.txt', 'w', encoding = 'utf-8') as f:
+            f.write(text) 
+            f.close()
+    except IOError:
+        print (" IO Error")
+
+def show():
+    print("Your ideabank: ")
+    try:
+        with open('ideabank.txt', 'r', encoding = 'utf-8') as f:
+            for x in enumerate(f,1):
+                tmp,tmp2 = x
+                print('{0}. {1}'.format(tmp, tmp2.strip()))
+    except IOError:
+        print("Error: File does not appear to exist.")
+
+def add( idea):
+    try:
+        with open('ideabank.txt', 'a', encoding = 'utf-8') as f:
+            f.write('\n' + idea)
+            f.close
+    except IOError:
+        print("Error: File does not appear to exist.")
+
+def dell(index):
+    try:
+        counter = 0
+        with open('ideabank.txt', 'r', encoding = 'utf-8') as f:
+            buffor = []
+            for line in f:
+                counter = counter + 1
+                #print(line)
+                if(counter != index):
+                    buffor.append(line)
+        f.close()
+        with open('ideabank.txt', 'w', encoding = 'utf-8') as k:
+            k.writelines(buffor)
+            k.close()
+    except IOError:
+        print("Error: File does not appear to exist.")
+    
+
+options, arg = getopt.getopt(sys.argv[1:], 'ld',['list','delete'])
+if(len(sys.argv) > 1):
+    for opt, arg in options:
+        if opt in ('-l', '--list'):
+            show()
+        elif opt in ('-d', '--delete'):
+            dell(int(sys.argv[2]))
+            show()
+else:
+    add(input("What is your new idea: "))
+    show()
